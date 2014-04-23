d236cfca099346e0a90636d3ca29b0ad819d6a38
Almaryen <aleksander.popel@gmail.com>
2014-04-23 14:34:02 +0300

    add MyException

---
+++ src/japplication3/MyException.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package japplication3;
+
+/**
+ *
+ * @author Ann
+ */
+public class MyException extends Exception  {
+    public MyException () {
+        super ();
+    }
+    public MyException (String msg) {
+        super (msg);
+    }
+}
+
+
